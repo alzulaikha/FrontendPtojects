8549a732278db75c7e5253d0d3d534125baea83a
alzulaikha <alzulaikha1992@gmail.com>
2026-08-18 15:08:48 +0400

    Completed Operators	&	Comparisons

--- LabTask3_Java/warmups.js
+++ LabTask3_Java/warmups.js
@@ -7,4 +7,13 @@ console.log( typeof num);
 num = "Hello";
 console.log( typeof num);
 
+//Operators	&	Comparisons
 
+cnsole.log('"10" == 10');
+console.log('"10" === 10');
+
+const emptyText = "";
+
+if (!emptyText) {
+    console.log("An	empty	string");
+}
