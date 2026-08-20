836ad10356931f7a900de8a08b5f85fe60dae36b
alzulaikha <alzulaikha1992@gmail.com>
2026-08-20 11:01:34 +0400

    Log .length	before	and	after	adding a duplicate entry

--- LabTask_Part1/warmups.js
+++ LabTask_Part1/warmups.js
@@ -11,3 +11,8 @@ console.log(cities);
 //unshift
 cities.unshift("Barka");
 console.log(cities);
+//Log .lenght befor adding a duplicate entry
+console.log("Cities before adding duplicate:", cities.length);
+//Log .lenght after adding a duplicate entry
+cities.push("Muscat");
+console.log("Cities after adding duplicate:", cities.length);
