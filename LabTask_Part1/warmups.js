8f7cda280ddeea5e344159c5e5b253fc940e8e72
alzulaikha <alzulaikha1992@gmail.com>
2026-08-20 10:38:23 +0400

    Build	an	array, using push / pop / shift /unshift

---
+++ LabTask_Part1/warmups.js
@@ -0,0 +1,13 @@
+const cities = ["Muscat", "Samail","Nizwa", "Sohar", "Sur","Salalah"];
+//push
+cities.push("Izki");
+console.log(cities);
+//pop
+cities.pop();
+console.log(cities);
+//shift
+cities.shift();
+console.log(cities);
+//unshift
+cities.unshift("Barka");
+console.log(cities);
